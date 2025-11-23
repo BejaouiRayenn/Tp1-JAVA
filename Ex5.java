224d50327e664e868c8a1bd4d0a1ebc0907f2197
Rayenn <bejaouiirayenn@gmail.com>
2025-11-23 19:43:59 +0100

    Ajout du programme de calcul de la factorielle Ex5

--- Ex5.java
+++ Ex5.java
@@ -1,23 +1,26 @@
 public class Ex5 {
-    public static void main(String[] args) {
+        public static void main(String[] args) {
         if (args.length != 1) {
             System.out.println("Usage: java Ex5 <entier>");
             return; 
+        }
         int nombre;
         try {
             nombre = Integer.parseInt(args[0]);
         } catch (NumberFormatException e) {
-            System.out.println(" L'argument doit être un entier valide.");
+            System.out.println("Erreur: L'argument doit être un entier valide.");
             return;
         }
         if (nombre < 0) {
-            System.out.println("NOMBRES Negatives !!.");
+            System.out.println("La factorielle n'est pas définie pour les nombres négatifs.");
             return;
         }
+
         if (nombre == 0 || nombre == 1) {
             System.out.println("La factorielle de " + nombre + " est 1");
             return;
         }
+        
         long factorielle = 1; 
         for (int i = 2; i <= nombre; i++) {
             factorielle = factorielle * i;
@@ -25,7 +28,3 @@ public class Ex5 {
         System.out.println("La factorielle de " + nombre + " est " + factorielle);
     }
 }
-
-}
-
-
