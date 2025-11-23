23c11a473af85a858d1336c96f4797804dccee7c
Rayenn <bejaouiirayenn@gmail.com>
2025-11-23 17:23:10 +0100

    Ajout du programme de l'exercice 2

--- EX2.java
+++ EX2.java
@@ -3,6 +3,6 @@ public static void main(String[] args) {
 float moyenne= 10.5f;
 System.out.println(25+25);
 System.out.println("25"+25);
-System.out.println("Mohamed"+1);
+System.out.println("Rayeeen"+1);
 System.out.println("La moyenne"+"de la classe="+moyenne+2);}
 }
